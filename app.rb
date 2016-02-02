1e900e06826486fc348b177e2c776926e932a35c
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-02 12:56:16 +0000

    challenge 12 completed, added params hash and p to server log

--- app.rb
+++ app.rb
@@ -18,7 +18,13 @@ get '/onthego' do
   'on the go!'
 end
 
-get '/cat' do
+get '/random-cat' do
   @pic_name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
